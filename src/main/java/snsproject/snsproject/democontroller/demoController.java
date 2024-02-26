118641500a777f411f4a39f70058a30e63dfac63
baekhun <baekhun2463@gmail.com>
2024-02-26 15:50:31 +0900

    CICD tese4

---
+++ src/main/java/snsproject/snsproject/democontroller/demoController.java
@@ -0,0 +1,13 @@
+package snsproject.snsproject.democontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class demoController {
+
+    @GetMapping("/demo")
+    public String demo() {
+        return "demo";
+    }
+}

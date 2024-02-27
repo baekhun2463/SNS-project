9415c4048ff0fd78a26eea3a281be244585f4fff
baekhun <baekhun2463@gmail.com>
2024-02-27 13:44:42 +0900

    tset

--- src/main/java/snsproject/snsproject/SnsProjectApplication.java
+++ src/main/java/snsproject/snsproject/SnsProjectApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SnsProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SnsProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SnsProjectApplication.class, args);
+    }
 
 }
--- src/test/java/snsproject/snsproject/SnsProjectApplicationTests.java
+++ src/test/java/snsproject/snsproject/SnsProjectApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SnsProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }

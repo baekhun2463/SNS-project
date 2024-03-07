00c996c0697cc258e17bd599d9b903d0d9dd4702
baekhun <baekhun2463@gmail.com>
2024-03-07 23:53:53 +0900

    delete and list

--- src/main/java/snsproject/snsproject/controller/PostController.java
+++ src/main/java/snsproject/snsproject/controller/PostController.java
@@ -1,6 +1,8 @@
 package snsproject.snsproject.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import snsproject.snsproject.controller.request.PostCreateRequest;
@@ -27,5 +29,21 @@ public class PostController {
     public Response<PostResponse> modify(@PathVariable Integer postId ,@RequestBody PostModifyRequest request, Authentication authentication) {
         Post post = postService.modify(request.getTitle(), request.getBody(), authentication.getName(), postId);
         return Response.success(PostResponse.fromPost(post));
+    }
+
+    @DeleteMapping("/{postId}")
+    public Response<Void> delete(@PathVariable Integer postId, Authentication authentication) {
+        postService.delete(authentication.getName(),postId);
+        return Response.success();
+    }
+
+    @GetMapping
+    public Response<Page<PostResponse>> list(Pageable pageable, Authentication authentication) {
+        return Response.success(postService.list(pageable).map(PostResponse::fromPost));
+    }
+
+    @GetMapping("/my")
+    public Response<Page<PostResponse>> my(Pageable pageable, Authentication authentication) {
+        return Response.success(postService.my(authentication.getName(), pageable).map(PostResponse::fromPost));
     }
 }
--- src/main/java/snsproject/snsproject/repository/PostEntityRepository.java
+++ src/main/java/snsproject/snsproject/repository/PostEntityRepository.java
@@ -1,12 +1,15 @@
 package snsproject.snsproject.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import snsproject.snsproject.model.entity.PostEntity;
+import snsproject.snsproject.model.entity.UserEntity;
 
 @Repository
 public interface PostEntityRepository extends JpaRepository<PostEntity, Integer> {
 
-
+    Page<PostEntity> findAllByUser(UserEntity entity, Pageable pageable);
 
 }
